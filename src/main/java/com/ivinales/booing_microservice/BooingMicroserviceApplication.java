20571056a8c01d88e4907ba9f196c1efeae327f6
ivinales <ian.vinales26@gmail.com>
2024-10-22 23:14:23 -0300

    init y conexion bd postgre

---
+++ src/main/java/com/ivinales/booing_microservice/BooingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ivinales.booing_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooingMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivinales/booing_microservice/BooingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivinales.booing_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
